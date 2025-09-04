ce4576b799fe663bb5b7370f86040e6ca60b94c0
chLi <565353780@qq.com>
2025-09-04 18:48:56 +0800

    create shape code dataset convert script

---
+++ dev_setup.sh
@@ -0,0 +1,24 @@
+cd ..
+git clone git@github.com:565353780/octree-shape.git
+git clone git@github.com:565353780/data-convert.git
+git clone git@github.com:565353780/base-trainer.git
+git clone git@github.com:565353780/dino-v2-detect.git
+
+cd octree-shape
+./dev_setup.sh
+
+cd ../data-convert
+./dev_setup.sh
+
+cd ../base-trainer
+./dev_setup.sh
+
+cd ../dino-v2-detect
+./dev_setup.sh
+
+pip install -U timm einops diffusers flow_matching thop torchcfm \
+  tos crcmod
+
+pip install -U cupy-cuda12x
+
+pip install flash-attn --no-build-isolation
---
+++ setup.sh
@@ -0,0 +1,22 @@
+cd ..
+git clone https://github.com/565353780/octree-shape.git
+git clone https://github.com/565353780/data-convert.git
+git clone https://github.com/565353780/base-trainer.git
+git clone https://github.com/565353780/dino-v2-detect.git
+
+cd octree-shape
+./dev_setup.sh
+
+cd ../data-convert
+./dev_setup.sh
+
+cd ../base-trainer
+./dev_setup.sh
+
+cd ../dino-v2-detect
+./dev_setup.sh
+
+pip install -U timm einops diffusers flow_matching thop torchcfm \
+  tos crcmod
+
+pip install -U cupy-cuda12x
